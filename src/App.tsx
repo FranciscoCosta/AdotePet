9b9886edc213229a2f2ae4ae07ff365f0563f9f0
Francisco Costa <francisco100eg@gmail.com>
2023-06-14 18:32:51 -0300

    Feat: Added react-router-dom to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 
 import './App.css'
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 function App() {
+
+  const Layout = () => {
+    return (
+        <>
+          <Outlet />
+        </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <div>Home</div> },
+      ],
+    },
+  ]);
+
 
   return (
     <div className="App">
+      <RouterProvider router={router} />
     </div>
   )
 }
